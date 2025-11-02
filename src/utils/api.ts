113cb3c72585a08403669738f86d180db420c52d
Monish Ranjan <monishranjan9@gmail.com>
2025-11-02 22:29:35 +0530

    fix: update API baseURL to use the correct production endpoint

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5555",
-    // baseURL: "https://dloklz-api.onrender.com",
+    // baseURL: "http://localhost:5555",
+    baseURL: "https://dloklz-api.onrender.com",
     headers: {
         "Content-Type": "application/json",
     },
